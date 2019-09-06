c39f3703fc833ac770dad2b3c3ceccdacd49962d
Wesleyss071299 <wesleysilvadesousa@gmail.com>
2019-09-06 12:48:11 -0300

    first commit

---
+++ app/components/Note.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { 
+  Text, 
+  View,
+  StyleSheet,
+  TouchableOpacity, 
+  Image 
+} from 'react-native';
+
+export default class HelloWorldApp extends Component {
+  render() {
+    return (
+      <View  key={this.props.keyval} style={styles.note}>
+        <Text style={styles.noteText}>{this.props.val.date}</Text>
+        <Text style={styles.noteText}>{this.props.val.note}</Text>
+        <TouchableOpacity onPress={this.props.deleteMethod} style={styles.noteDelete}>
+          <Image source={require('./baseline_delete_black_18dp.png')}/>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  note: {
+    position: 'relative',
+    padding: 20,
+    paddingRight:100,
+    borderBottomWidth: 2,
+    borderBottomColor: '#ededed',
+  },
+  noteText: {
+    paddingLeft: 20,
+    borderLeftWidth: 10,
+    borderLeftColor: '#e91e63',
+  },
+  noteDelete: {
+    position: 'absolute',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#2980b9',
+    padding: 10,
+    top: 10,
+    bottom: 10,
+    right: 10
+  },
+  noteDeleteText: {
+    color: 'white',
+  } 
+});
